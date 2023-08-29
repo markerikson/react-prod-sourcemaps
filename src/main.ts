4b0983353fd32a1ee05c256649c777f86a01eba8
Mark Erikson <mark@isquaredsoftware.com>
2023-08-28 22:15:28 -0400

    Initial CLI usage

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,27 @@
 #!/usr/bin/env node
+
+import yargs from "yargs";
+
 import { knownReactProdVersions } from "./reactVersions";
 
-console.log("React versions: ", knownReactProdVersions);
+const argv = yargs
+  .option("reactVersions", {
+    alias: "rv",
+    description: "Print available React versions",
+    type: "boolean",
+  })
+  .option("target", {
+    alias: "t",
+    default: "all",
+    description: "Only re-generate tests for this target",
+    choices: ["all", "browser", "node"],
+  })
+  .help()
+  .alias("help", "h")
+  .parseSync();
+
+if (argv.reactVersions) {
+  console.log("Available React versions:", knownReactProdVersions);
+} else {
+  console.log("Goodbye!");
+}
